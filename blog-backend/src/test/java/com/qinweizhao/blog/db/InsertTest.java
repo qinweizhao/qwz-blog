153fb0b29173dae729ce34733bdf54b586a58edf
YVKG <yvkg@qq.com>
2023-05-22 13:02:58 +0800

    commit

---
+++ blog-backend/src/test/java/com/qinweizhao/blog/db/InsertTest.java
@@ -0,0 +1,62 @@
+package com.qinweizhao.blog.db;
+
+import com.qinweizhao.blog.mapper.CommentMapper;
+import com.qinweizhao.blog.model.entity.Comment;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author qinweizhao
+ * @since 2023-05-22
+ */
+@SpringBootTest
+class InsertTest {
+
+    @Autowired
+    CommentMapper commentMapper;
+
+
+    @Test
+    @Transactional
+    void test() {
+        Comment comment = new Comment();
+        comment.setId(0L);
+        comment.setType(0);
+        comment.setAllowNotification(false);
+        comment.setAuthor("");
+        comment.setAuthorUrl("");
+        comment.setContent("");
+        comment.setEmail("");
+        comment.setGravatarMd5("");
+        comment.setIpAddress("");
+        comment.setIsAdmin(false);
+        comment.setParentId(0L);
+        comment.setTargetId(0);
+        comment.setStatus(0);
+        comment.setTopPriority(0);
+        comment.setUserAgent("");
+        comment.setCreateTime(LocalDateTime.now());
+        comment.setUpdateTime(LocalDateTime.now());
+
+        int insert = commentMapper.insert(comment);
+
+        if (insert == 1) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // 模拟异常
+        int i = 10 / 0;
+
+        System.out.println("程序结束");
+
+    }
+
+}
