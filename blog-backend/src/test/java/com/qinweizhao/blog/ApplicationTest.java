3fcda9fa954088c0a91ab851f70e6d0e40a04655
YVKG <yvkg@qq.com>
2023-05-28 21:52:28 +0800

    commit

--- blog-backend/src/test/java/com/qinweizhao/blog/ApplicationTest.java
+++ blog-backend/src/test/java/com/qinweizhao/blog/ApplicationTest.java
@@ -1,8 +1,13 @@
 package com.qinweizhao.blog;
 
 import cn.hutool.crypto.digest.BCrypt;
+import com.qinweizhao.blog.config.properties.MyBlogProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author qinweizhao
@@ -12,10 +17,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApplicationTest {
 
 
+    @Autowired
+    private MyBlogProperties myBlogProperties;
+
     @Test
     void generatorPassword() {
         String password = "admin";
         System.out.println(BCrypt.hashpw(password, BCrypt.gensalt()));
+    }
+    @Test
+    void portalPath(){
+        String frontendDirName = myBlogProperties.getFrontendDirName();
+        String themeDirName = myBlogProperties.getThemeDirName();
+        Path frontend = Paths.get(myBlogProperties.getWorkDir(), frontendDirName,themeDirName);
+        System.out.println(frontendDirName);
+        System.out.println("frontend = " + frontend);
     }
 
 }
